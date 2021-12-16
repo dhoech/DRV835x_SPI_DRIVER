239a541aaf87123f50c7eaa368af6107c27fcab9
Jan190 <jan.jakovljevic@gmail.com>
2021-12-16 21:56:27 +0100

    Test DRV835x_CmdWrite i DRV835x_CmdRead

--- N-G431RB_DRV835x_SPI_DRIVER/Core/Inc/DRV835x_SPI_DRIVER.h
+++ N-G431RB_DRV835x_SPI_DRIVER/Core/Inc/DRV835x_SPI_DRIVER.h
@@ -27,22 +27,31 @@ typedef struct{
  * INICIJALIZACIJA
  */
 
-uint8_t GSV6_Initialise(DRV835x* device, SPI_HandleTypeDef* SPIHandle);
+uint8_t DRV835x_Initialise(DRV835x* device, SPI_HandleTypeDef* SPIHandle);
 
 /*
  * AKVIZICIJA PODATAKA I SLANJE NAREDBI
  */
 
-HAL_StatusTypeDef DRV835x_FaultStatus(DRV835x* device);
-HAL_StatusTypeDef DRV835x_CmdWrite(DRV835x* device, uint8_t address, uint8_t MsgTx);
-HAL_StatusTypeDef DRV835x_CmdRead(DRV835x* device, uint8_t address, uint8_t MsgRx);
+/*
+ * 									DEFINICIJA PORUKE
+ * 					-------------------------------------------------
+ *					|	15 bit		|	14-11 bit	|	10-0 bit 	|
+ * 					-------------------------------------------------
+ * 					|	Read/write	|				|				|
+ * 					|	1 = read	|	Addressa	|	  Data		|
+ * 					|	0 = write	|				|				|
+ * 					-------------------------------------------------
+ */
+
+HAL_StatusTypeDef DRV835x_CmdRead(DRV835x* device, uint8_t address, uint8_t *MsgRx);
+HAL_StatusTypeDef DRV835x_CmdWrite(DRV835x* device, uint8_t address, uint16_t MsgTx);
 
 /*
  * LOW-LEVEL FUNKCIJE
  *
  */
-HAL_StatusTypeDef DRV835x_SPI_TransmitReceive(DRV835x* device, uint8_t *pTxData, uint8_t *pRxData);
+HAL_StatusTypeDef DRV835x_SPI_TransmitReceive(DRV835x* device, uint8_t *MsgTx, uint8_t *MsgRx);
 HAL_StatusTypeDef DRV835x_SPI_Transmit(DRV835x* device, uint8_t* MsgTx);
-HAL_StatusTypeDef DRV835x_SPI_Receive(DRV835x* device, uint8_t* MsgRx);
 
 #endif /* INC_DRV835X_SPI_DRIVER_H_ */
--- N-G431RB_DRV835x_SPI_DRIVER/Core/Src/DRV835x_SPI_DRIVER.c
+++ N-G431RB_DRV835x_SPI_DRIVER/Core/Src/DRV835x_SPI_DRIVER.c
@@ -8,14 +8,57 @@
 #include "DRV835x_SPI_DRIVER.h"
 
 
-uint8_t GSV6_Initialise(DRV835x* device, SPI_HandleTypeDef* SpiHandle){
+uint8_t DRV835x_Initialise(DRV835x* device, SPI_HandleTypeDef* SpiHandle){
 
+	/*Deklaracija parametara unutar structa za SPI izlaz*/
 	device->SPIHandle = SpiHandle;
+
+	/* Varijable za spremanje broja errora (vracaju se nakon sto se izvrti cijela funkcija)*/
+	uint8_t errNum = 0;
+/*	HAL_StatusTypeDef status;*/
+
+	/*Bufferi*/
+/*	uint8_t MsgTx[2], MsgRx[2];*/
+
+
+
+/*	DRV835x_SPI_TransmitReceive(device, &MsgTx, &MsgRx);*/
+
+	return errNum;
+}
+
+/*
+ * AKVIZICIJA PODATAKA I SLANJE NAREDBI
+ */
+
+HAL_StatusTypeDef DRV835x_CmdRead(DRV835x* device, uint8_t address, uint8_t *MsgRx){
+
+	uint8_t MsgTx[2];
+
+	/* Pisanje poruke za read addresse*/
+	MsgTx[0] = (address << 3) & 0x78;
+	MsgTx[0] = MsgTx[0] | 0x80;
+	MsgTx[1] = 0x00;
+
+	DRV835x_SPI_TransmitReceive(device, MsgTx, MsgRx);
 
 	return 0;
 }
 
+HAL_StatusTypeDef DRV835x_CmdWrite(DRV835x* device, uint8_t address, uint16_t MsgTx){
 
+	uint8_t Msg[2];
+
+	/* Pisanje poruke za write addresse*/
+	Msg[0] = (address << 3) & 0x78;
+	Msg[0] = Msg[0] | ((MsgTx >> 8) & 0x07 );
+	Msg[0] = Msg[0] & 0x7F;
+	Msg[1] = MsgTx & 0xFF;
+
+	DRV835x_SPI_Transmit(device, Msg);
+
+	return 0;
+}
 
 
 
@@ -33,9 +76,4 @@ HAL_StatusTypeDef DRV835x_SPI_TransmitReceive(DRV835x* device, uint8_t *MsgTx, u
 HAL_StatusTypeDef DRV835x_SPI_Transmit(DRV835x* device, uint8_t* MsgTx){
 
 	return HAL_SPI_Transmit(device->SPIHandle, MsgTx, 2, HAL_MAX_DELAY);
-}
-
-HAL_StatusTypeDef DRV835x_SPI_Receive(DRV835x* device, uint8_t* MsgRx){
-
-	return HAL_SPI_Receive(device->SPIHandle, MsgRx, 2, HAL_MAX_DELAY);
 }
